5eaec3d913ca6abab55ace2c7774a6a0311a745b
= <=>
2025-03-01 17:20:31 +0700

    ui+ : add sections and links inside sidebar.

---
+++ src/components/ui/SidebarSectionComponent.svelte
@@ -0,0 +1,19 @@
+<script>
+    let {name} = $props()   
+</script>
+
+<h4>{name}</h4>
+
+<style>
+    h4{
+        /* margin-top: 3rem; */
+        color: rgb(107, 103, 103);
+        font-size: 0.7em;
+    }
+    /* h4:nth-child(2),
+    h4:nth-child(3){
+        margin-top: 3rem;
+        color: gray;
+        font-size: 0.5em;
+    } */
+</style>
